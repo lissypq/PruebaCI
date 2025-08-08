94633d4403d1016e3a35df33dc70032d985a91cc
lissypq <lissy0147@hotmail.com>
2025-08-07 22:45:58 -0400

    Agrega componente contact-page

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ContactPage } from './contact-page/contact-page';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,ContactPage],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
